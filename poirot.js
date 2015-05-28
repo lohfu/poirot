19c4d296a7930aa294103d7f664f4896d7382358
Linus Miller <linus.miller@thecodebureau.com>
2015-05-28 17:49:32 +0200

    Refactored a lot, but exactly the same functionality. Except that i added toDotCase.

--- poirot.js
+++ poirot.js
@@ -4,17 +4,23 @@ var p = String.prototype;
 function normalize(str) {
 	var arr = str.split(/[\s-_.]/);
 
-	if(arr.length > 1)
-		return arr.map(function(part) { return part.toLowerCase(); }).join('-');
-	else
-		return (str.charAt(0).toLowerCase() + str.slice(1)).replace(/([A-Z])/, '-$&').toLowerCase();
+	if(arr.length < 2) {
+		var matches = str.match(/([A-Z])/g);
+		arr = str.split(/[A-Z]/).map(function(part, index) {
+			if(index > 0) part = matches[index - 1] + part;
+			
+			return part.toLowerCase();
+		});
+		if(!arr[0]) arr.shift();
+	} else
+		arr = arr.map(function(part) { return part.toLowerCase(); });
+
+	return arr;
 }
 
 // Converts spinal-case, snake_case or space case to camelCase
 p.toCamelCase = function(pascalCase) {
-	var str = this.toLowerCase();
-
-	var arr = str.split(/[\s-_]/);
+	var arr = normalize(this);
 
 	for(var i = pascalCase ? 0 : 1; i < arr.length; i++) {
 		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
@@ -25,21 +31,25 @@ p.toCamelCase = function(pascalCase) {
 
 // Converts spinal-case, snake_case or space case to PascalCase
 p.toPascalCase = function() {
-	return normalize(this).toCamelCase(true);
+	return this.toCamelCase(true);
 };
 
 // converts camelCase or PascalCase to spinal-case/
 p.toSpinalCase = function() {
-	return normalize(this);
+	return normalize(this).join('-');
+};
+
+p.toDotCase = function() {
+	return normalize(this).join('.');
 };
 
 // converts camelCase or PascalCase to snake_case/
 p.toSnakeCase = function() {
-	return normalize(this).split('-').join('_');
+	return normalize(this).join('_');
 };
 
-p.toSpaceCase = function(capitals) {
-	return normalize(this).split('-').map(function(part) {
+p.toSpaceCase = function(capitals, divider) {
+	return normalize(this).map(function(part) {
 		return capitals ? part.charAt(0).toUpperCase() + part.slice(1) : part;
 	}).join(' ');
 };
