2af89eecb3d86337ed2eac28e5db3b1f6d156de7
Linus Miller <lohfu@lohfu.io>
2015-10-22 14:58:24 +0200

    No more extending String.prototype.

--- poirot.js
+++ poirot.js
@@ -1,10 +1,10 @@
-var p = String.prototype;
-
 // normalize always returns the string in spinal-case
 function normalize(str) {
+	// TODO trim
 	var arr = str.split(/[\s-_.]/);
 
 	if(arr.length < 2) {
+		// TODO allow other characters eg ÅÄÖ
 		var matches = str.match(/([A-Z])/g);
 		arr = str.split(/[A-Z]/).map(function(part, index) {
 			if(index > 0) part = matches[index - 1] + part;
@@ -18,38 +18,51 @@ function normalize(str) {
 	return arr;
 }
 
-// Converts spinal-case, snake_case or space case to camelCase
-p.toCamelCase = function(pascalCase) {
-	var arr = normalize(this);
+function camelCase(str) {
+	var arr = normalize(str);
 
-	for(var i = pascalCase ? 0 : 1; i < arr.length; i++) {
+	for(var i = 1; i < arr.length; i++) {
 		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
 	}
 
 	return arr.join('');
-};
+}
 
-// Converts spinal-case, snake_case or space case to PascalCase
-p.toPascalCase = function() {
-	return this.toCamelCase(true);
-};
+function pascalCase(str) {
+	var arr = normalize(str);
 
-// converts camelCase or PascalCase to spinal-case/
-p.toSpinalCase = function() {
-	return normalize(this).join('-');
-};
+	for(var i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+	}
 
-p.toDotCase = function() {
-	return normalize(this).join('.');
-};
+	return arr.join('');
+}
 
-// converts camelCase or PascalCase to snake_case/
-p.toSnakeCase = function() {
-	return normalize(this).join('_');
-};
+function spinalCase(str) {
+	return normalize(str).join('-');
+}
 
-p.toSpaceCase = function(capitals, divider) {
-	return normalize(this).map(function(part) {
+function dotCase(str) {
+	return normalize(str).join('.');
+}
+
+function snakeCase(str) {
+	return normalize(str).join('_');
+}
+
+function spaceCase(str, capitals, divider) {
+	return normalize(str).map(function(part) {
 		return capitals ? part.charAt(0).toUpperCase() + part.slice(1) : part;
 	}).join(' ');
+}
+
+module.exports = {
+	camelCase: camelCase,
+	dotCase: dotCase,
+	pascalCase: pascalCase,
+	spinalCase: spinalCase,
+	kebabCase: spinalCase,
+	snakeCase: snakeCase,
+	spaceCase: spaceCase
 };
+
